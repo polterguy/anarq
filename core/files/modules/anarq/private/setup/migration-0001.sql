cdf0b73881285cce04437114c486826d70e0119d
Thomas Hansen <thomas@gaiasoul.com>
2020-06-22 17:51:56 +0300

    Bettering

--- core/files/modules/anarq/private/setup/migration-0001.sql
+++ core/files/modules/anarq/private/setup/migration-0001.sql
@@ -14,6 +14,14 @@ insert into translations (`locale`, `key`, `content`) values ('en', 'AuditYourVo
 update translations set `content` = 'Registrer' where `key` = 'Register' and `locale` = 'no';
 update translations set `content` = 'Lukk' where `key` = 'Home' and `locale` = 'no';
 update translations set `content` = 'Neste' where `key` = 'Get25NextItems' and `locale` = 'no';
+update translations set `content` = 'Open for {0} days' where `key` = 'CaseOpenShort' and `locale` = 'en';
+update translations set `content` = 'Open for {0} days' where `key` = 'CaseOpenMedium' and `locale` = 'en';
+update translations set `content` = 'Open for {0} days' where `key` = 'CaseOpenLong' and `locale` = 'en';
+update translations set `content` = 'Åpen i {0} dager' where `key` = 'CaseOpenShort' and `locale` = 'no';
+update translations set `content` = 'Åpen i {0} dager' where `key` = 'CaseOpenMedium' and `locale` = 'no';
+update translations set `content` = 'Åpen i {0} dager' where `key` = 'CaseOpenLong' and `locale` = 'no';
+
+
 
 /*
  * Deletions.
@@ -21,5 +29,7 @@ update translations set `content` = 'Neste' where `key` = 'Get25NextItems' and `
 delete from translations where `key` = 'HomeSlogan';
 delete from translations where `key` = 'fromNow';
 delete from translations where `key` = 'StartPoliticalRally';
+
+
 
 
